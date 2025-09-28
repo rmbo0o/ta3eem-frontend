1f7e0ee4eaf49495a06523884211601233fa0153
rmbo0o <rmboo25@gmail>
2025-09-28 13:19:44 -0700

    fix the catogery url

--- src/components/CategoryCards.vue
+++ src/components/CategoryCards.vue
@@ -52,8 +52,8 @@
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-const API_BASE_URL ='https://ta3eem-frontendnew.onrender.com'
-//  import.meta.env.VITE_API_URL ||
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://ta3eem-frontendnew.onrender.com'
+
 // Reactive data
 const categories = ref([
   { id: 1, name: 'Breakfast' },
@@ -76,7 +76,7 @@ async function fetchCategoryItems(categoryId, categoryName) {
   categoryItems.value = [] // Clear previous items
 
   try {
-    const response = await axios.get(`${API_BASE_URL}/api/categories/food-items`, {
+    const response = await axios.get(`${API_BASE_URL}/categories/food-items`, {
       params: {
         category_id: categoryId,
         category_name: categoryName,
